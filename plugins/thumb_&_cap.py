03b1345d1501465bdc2bc83bcfb048f50fa2d616
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2024-05-31 20:04:38 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -37,13 +37,13 @@ async def viewthumb(client, message):
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
 async def removethumb(client, message):
     await madflixbotz.set_thumbnail(message.from_user.id, file_id=None)
-    await message.reply_text("**ᴛʜᴜᴍʙɴᴀɪʟ ᴅᴇʟᴇᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ 🗑️**")
+    await message.reply_text("**ᴛʜᴜᴍʙɴᴀɪʟ ᴅᴇʟᴇᴛᴇᴅ 🗑️**")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
     mkn = await message.reply_text("Please Wait ...")
     await madflixbotz.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
-    await mkn.edit("**<b>ᴛʜᴜᴍʙɴᴀɪʟ sᴀᴠᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ✓**")
+    await mkn.edit("**Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ ✓**")
 
 
 
